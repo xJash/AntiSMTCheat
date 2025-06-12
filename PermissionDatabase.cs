efda02b06e90d90881123a8bcfa3029e70d18fba
Zachary Mitchell <zackmitchell1211@live.com>
2025-06-11 22:45:51 -0400

    Finished up & posted on discord

--- PermissionDatabase.cs
+++ PermissionDatabase.cs
@@ -13,28 +13,28 @@ public static class PermissionDatabase {
     public static readonly IReadOnlyDictionary<string, HashSet<CommandPermission>> CommandPermissionMap = new Dictionary<string, HashSet<CommandPermission>>
         {
             // AchievementsManager
-            { "AchievementsManager.InvokeUserCode_CmdAddAchievementPoint__Int32__Int32", new HashSet<CommandPermission> { CommandPermission.General, CommandPermission.Restocker } },
-            { "AchievementsManager.InvokeUserCode_CmdMaxFundsCheckouted__Single", new HashSet<CommandPermission> { CommandPermission.General } },
+            { "AchievementsManager.InvokeUserCode_CmdAddAchievementPoint__Int32__Int32", new HashSet<CommandPermission> { CommandPermission.General, CommandPermission.Restocker, CommandPermission.Manager, CommandPermission.Cashier } },
+            { "AchievementsManager.InvokeUserCode_CmdMaxFundsCheckouted__Single", new HashSet<CommandPermission> { CommandPermission.Cashier } },
             { "AchievementsManager.InvokeUserCode_CmdRequestGachaponSphere__Int32", new HashSet<CommandPermission> { CommandPermission.General } },
             { "AchievementsManager.InvokeUserCode_CmdRequestGachaponDeletion__GameObject", new HashSet<CommandPermission> { CommandPermission.General } },
 
             // CardboardBaler
-            { "CardboardBaler.InvokeUserCode_CmdAddBoxToBaler", new HashSet<CommandPermission> { CommandPermission.General } },
+            { "CardboardBaler.InvokeUserCode_CmdAddBoxToBaler", new HashSet<CommandPermission> { CommandPermission.Restocker } },
 
             // Data_Container
-            { "Data_Container.InvokeUserCode_CmdUpdateArrayValues__Int32__Int32__Int32", new HashSet<CommandPermission> { CommandPermission.General } },
+            { "Data_Container.InvokeUserCode_CmdUpdateArrayValues__Int32__Int32__Int32", new HashSet<CommandPermission> { CommandPermission.Restocker } },
             { "Data_Container.InvokeUserCode_CmdContainerClear__Int32", new HashSet<CommandPermission> { CommandPermission.General } },
-            { "Data_Container.InvokeUserCode_CmdUpdateArrayValuesStorage__Int32__Int32__Int32", new HashSet<CommandPermission> { CommandPermission.General } },
-            { "Data_Container.InvokeUserCode_CmdActivateCashMethod__Int32", new HashSet<CommandPermission> { CommandPermission.General } },
-            { "Data_Container.InvokeUserCode_CmdActivateCreditCardMethod", new HashSet<CommandPermission> { CommandPermission.General } },
-            { "Data_Container.InvokeUserCode_CmdReceivePayment__Single", new HashSet<CommandPermission> { CommandPermission.General } },
+            { "Data_Container.InvokeUserCode_CmdUpdateArrayValuesStorage__Int32__Int32__Int32", new HashSet<CommandPermission> { CommandPermission.General, CommandPermission.Restocker} },
+            { "Data_Container.InvokeUserCode_CmdActivateCashMethod__Int32", new HashSet<CommandPermission> { CommandPermission.Cashier } },
+            { "Data_Container.InvokeUserCode_CmdActivateCreditCardMethod", new HashSet<CommandPermission> { CommandPermission.Cashier } },
+            { "Data_Container.InvokeUserCode_CmdReceivePayment__Single", new HashSet<CommandPermission> { CommandPermission.Cashier } },
             { "Data_Container.InvokeUserCode_CmdResetFirstCustomer", new HashSet<CommandPermission> { CommandPermission.General } },
             { "Data_Container.InvokeUserCode_CmdCloseCheckout", new HashSet<CommandPermission> { CommandPermission.Cashier, CommandPermission.Manager } },
             { "Data_Container.InvokeUserCode_CmdRequestCloseState", new HashSet<CommandPermission> { CommandPermission.General } },
 
             // DebtManager
             { "DebtManager.InvokeUserCode_CmdRequestInvoiceGeneration", new HashSet<CommandPermission> { CommandPermission.General } },
-            { "DebtManager.InvokeUserCode_CmdRequestLoan__Int32__Int32__Int32", new HashSet<CommandPermission> { CommandPermission.General } },
+            { "DebtManager.InvokeUserCode_CmdRequestLoan__Int32__Int32__Int32", new HashSet<CommandPermission> { CommandPermission.Manager } },
             { "DebtManager.InvokeUserCode_CmdPayInvoice__Int32", new HashSet<CommandPermission> { CommandPermission.General } },
 
             // DecorationExtraData
@@ -65,11 +65,11 @@ public static class PermissionDatabase {
             { "EventCageSpawnNets.InvokeUserCode_CmdDepositAnimalInside__Int32", new HashSet<CommandPermission> { CommandPermission.General } },
 
             // GameData
-            { "GameData.InvokeUserCode_CmdAlterFunds__Single", new HashSet<CommandPermission> { CommandPermission.Manager } },
-            { "GameData.InvokeUserCode_CmdAlterFundsWithoutExperience__Single", new HashSet<CommandPermission> { CommandPermission.General } },
+            { "GameData.InvokeUserCode_CmdAlterFunds__Single", new HashSet<CommandPermission> { CommandPermission.Manager, CommandPermission.Restocker, CommandPermission.Security, CommandPermission.Cashier } },
+            { "GameData.InvokeUserCode_CmdAlterFundsWithoutExperience__Single", new HashSet<CommandPermission> { CommandPermission.General, CommandPermission.Manager } },
             { "GameData.InvokeUserCode_CmdAcquireFranchise__Int32", new HashSet<CommandPermission> { CommandPermission.Manager } },
-            { "GameData.InvokeUserCode_CmdOpenSupermarket", new HashSet<CommandPermission> { CommandPermission.General } },
-            { "GameData.InvokeUserCode_CmdEndDayFromButton", new HashSet<CommandPermission> { CommandPermission.General } },
+            { "GameData.InvokeUserCode_CmdOpenSupermarket", new HashSet<CommandPermission> { CommandPermission.Manager } },
+            { "GameData.InvokeUserCode_CmdEndDayFromButton", new HashSet<CommandPermission> { CommandPermission.Manager } },
             { "GameData.InvokeUserCode_CmdmoneySpentOnProducts__Single", new HashSet<CommandPermission> { CommandPermission.Manager } },
             { "GameData.InvokeUserCode_CmdHostDisconnect", new HashSet<CommandPermission> { CommandPermission.General } },
 
@@ -79,14 +79,14 @@ public static class PermissionDatabase {
             // ManagerBlackboard
             { "ManagerBlackboard.InvokeUserCode_CmdAddProductToSpawnList__Int32", new HashSet<CommandPermission> { CommandPermission.Manager } },
             { "ManagerBlackboard.InvokeUserCode_CmdSpawnBoxEmpty", new HashSet<CommandPermission> { CommandPermission.Manager } },
-            { "ManagerBlackboard.InvokeUserCode_CmdSpawnBoxFromPlayer__Vector3__Int32__Int32__Single", new HashSet<CommandPermission> { CommandPermission.General } },
+            { "ManagerBlackboard.InvokeUserCode_CmdSpawnBoxFromPlayer__Vector3__Int32__Int32__Single", new HashSet<CommandPermission> { CommandPermission.Restocker } },
 
             // MiniTransportBehaviour
-            { "MiniTransportBehaviour.InvokeUserCode_CmdRequestOwnership__GameObject", new HashSet<CommandPermission> { CommandPermission.General } },
+            { "MiniTransportBehaviour.InvokeUserCode_CmdRequestOwnership__GameObject", new HashSet<CommandPermission> { CommandPermission.Restocker } },
             { "MiniTransportBehaviour.InvokeUserCode_CmdRemoveOwnership", new HashSet<CommandPermission> { CommandPermission.General } },
             { "MiniTransportBehaviour.InvokeUserCode_CmdOnPeopleHit__Vector3", new HashSet<CommandPermission> { CommandPermission.General } },
-            { "MiniTransportBehaviour.InvokeUserCode_CmdCollisionHit__Vector3__Single", new HashSet<CommandPermission> { CommandPermission.General } },
-            { "MiniTransportBehaviour.InvokeUserCode_CmdHorn", new HashSet<CommandPermission> { CommandPermission.General } },
+            { "MiniTransportBehaviour.InvokeUserCode_CmdCollisionHit__Vector3__Single", new HashSet<CommandPermission> { CommandPermission.Restocker } },
+            { "MiniTransportBehaviour.InvokeUserCode_CmdHorn", new HashSet<CommandPermission> { CommandPermission.Restocker } },
             { "MiniTransportBehaviour.InvokeUserCode_CmdUpdateArrayValuesStorage__Int32__Int32__Int32", new HashSet<CommandPermission> { CommandPermission.General } },
 
             // MisterGift
@@ -96,25 +96,25 @@ public static class PermissionDatabase {
             { "MisterGrusch.InvokeUserCode_CmdHitFromPlayer", new HashSet<CommandPermission> { CommandPermission.General } },
 
             // NetworkSpawner
-            { "NetworkSpawner.InvokeUserCode_CmdSpawn__Int32__Vector3__Vector3", new HashSet<CommandPermission> { CommandPermission.General } },
+            { "NetworkSpawner.InvokeUserCode_CmdSpawn__Int32__Vector3__Vector3", new HashSet<CommandPermission> { CommandPermission.Manager } },
             { "NetworkSpawner.InvokeUserCode_CmdSpawnProp__Int32__Vector3__Vector3", new HashSet<CommandPermission> { CommandPermission.General } },
             { "NetworkSpawner.InvokeUserCode_CmdSpawnDecoration__Int32__Vector3__Vector3", new HashSet<CommandPermission> { CommandPermission.General } },
-            { "NetworkSpawner.InvokeUserCode_CmdObjectMove__GameObject__Vector3__Quaternion", new HashSet<CommandPermission> { CommandPermission.General } },
+            { "NetworkSpawner.InvokeUserCode_CmdObjectMove__GameObject__Vector3__Quaternion", new HashSet<CommandPermission> { CommandPermission.Manager } },
             { "NetworkSpawner.InvokeUserCode_CmdSpawnBox__Int32__Vector3__Int32__Int32", new HashSet<CommandPermission> { CommandPermission.General } },
             { "NetworkSpawner.InvokeUserCode_CmdSpawnTrayFromPlayer__Vector3__String__Single", new HashSet<CommandPermission> { CommandPermission.General } },
             { "NetworkSpawner.InvokeUserCode_CmdSpawnOrderBoxFromPlayer__Vector3__Single__String__String__String", new HashSet<CommandPermission> { CommandPermission.General } },
-            { "NetworkSpawner.InvokeUserCode_CmdDestroyBox__GameObject", new HashSet<CommandPermission> { CommandPermission.General } },
+            { "NetworkSpawner.InvokeUserCode_CmdDestroyBox__GameObject", new HashSet<CommandPermission> { CommandPermission.Restocker, CommandPermission.General, CommandPermission.Manager} },
             { "NetworkSpawner.InvokeUserCode_CmdSetSupermarketText__String", new HashSet<CommandPermission> { CommandPermission.Manager } },
             { "NetworkSpawner.InvokeUserCode_CmdSetSupermarketColor__Color", new HashSet<CommandPermission> { CommandPermission.General } },
 
             // PlayerNetwork
             { "PlayerNetwork.InvokeUserCode_CmdCrouch__Boolean", new HashSet<CommandPermission> { CommandPermission.General } },
-            { "PlayerNetwork.InvokeUserCode_CmdSetBoxColorToEveryone__Int32", new HashSet<CommandPermission> { CommandPermission.General } },
-            { "PlayerNetwork.InvokeUserCode_CmdUpdateTrayToEveryone__String", new HashSet<CommandPermission> { CommandPermission.General } },
+            { "PlayerNetwork.InvokeUserCode_CmdSetBoxColorToEveryone__Int32", new HashSet<CommandPermission> { CommandPermission.Restocker } },
+            { "PlayerNetwork.InvokeUserCode_CmdUpdateTrayToEveryone__String", new HashSet<CommandPermission> { CommandPermission.Restocker } },
             { "PlayerNetwork.InvokeUserCode_CmdChangeCharacter__Int32", new HashSet<CommandPermission> { CommandPermission.General } },
             { "PlayerNetwork.InvokeUserCode_CmdChangeHat__Int32", new HashSet<CommandPermission> { CommandPermission.General } },
-            { "PlayerNetwork.InvokeUserCode_CmdChangeEquippedItem__Int32", new HashSet<CommandPermission> { CommandPermission.General } },
-            { "PlayerNetwork.InvokeUserCode_CmdPlayAnimation__Int32", new HashSet<CommandPermission> { CommandPermission.Manager } },
+            { "PlayerNetwork.InvokeUserCode_CmdChangeEquippedItem__Int32", new HashSet<CommandPermission> { CommandPermission.Restocker, CommandPermission.General } },
+            { "PlayerNetwork.InvokeUserCode_CmdPlayAnimation__Int32", new HashSet<CommandPermission> { CommandPermission.Manager, CommandPermission.Security, CommandPermission.General } },
             { "PlayerNetwork.InvokeUserCode_CmdDebtCollectorAchievement", new HashSet<CommandPermission> { CommandPermission.General } },
             { "PlayerNetwork.InvokeUserCode_CmdPushPlayer__Vector3", new HashSet<CommandPermission> { CommandPermission.General } },
             { "PlayerNetwork.InvokeUserCode_CmdPlayPricingSound", new HashSet<CommandPermission> { CommandPermission.General } },
@@ -129,7 +129,7 @@ public static class PermissionDatabase {
             { "NetworkGameBehaviors.InvokeUserCode_CmdRefreshStatus", new HashSet<CommandPermission> { CommandPermission.General } },
 
             // NPC_Info
-            { "NPC_Info.InvokeUserCode_CmdAnimationPlay__Int32", new HashSet<CommandPermission> { CommandPermission.General } },
+            { "NPC_Info.InvokeUserCode_CmdAnimationPlay__Int32", new HashSet<CommandPermission> { CommandPermission.Security, CommandPermission.General } },
             { "NPC_Info.InvokeUserCode_CmdSurveillanceSet", new HashSet<CommandPermission> { CommandPermission.Security, CommandPermission.Manager } },
 
             // NPC_Manager
@@ -139,11 +139,11 @@ public static class PermissionDatabase {
             { "NPC_Manager.InvokeUserCode_CmdHireEmployeeData__Int32__String", new HashSet<CommandPermission> { CommandPermission.Manager } },
             { "NPC_Manager.InvokeUserCode_CmdDismissEmployeeData__Int32", new HashSet<CommandPermission> { CommandPermission.Manager } },
             { "NPC_Manager.InvokeUserCode_CmdRequestHappinessLevel", new HashSet<CommandPermission> { CommandPermission.General } },
-            { "NPC_Manager.InvokeUserCode_CmdChangeEmployeeName__Int32__String", new HashSet<CommandPermission> { CommandPermission.General } },
+            { "NPC_Manager.InvokeUserCode_CmdChangeEmployeeName__Int32__String", new HashSet<CommandPermission> { CommandPermission.Manager } },
             { "NPC_Manager.InvokeUserCode_CmdChangeEmployeeHat__Int32__Int32", new HashSet<CommandPermission> { CommandPermission.General } },
-            { "NPC_Manager.InvokeUserCode_CmdChangeEmployeePriority__Int32__Int32", new HashSet<CommandPermission> { CommandPermission.General } },
+            { "NPC_Manager.InvokeUserCode_CmdChangeEmployeePriority__Int32__Int32", new HashSet<CommandPermission> { CommandPermission.Manager } },
             { "NPC_Manager.InvokeUserCode_CmdChangeAllEmployeePriorities__Int32", new HashSet<CommandPermission> { CommandPermission.General } },
-            { "NPC_Manager.InvokeUserCode_CmdLoadPrioritiesLayout__Int32[]", new HashSet<CommandPermission> { CommandPermission.General } },
+            { "NPC_Manager.InvokeUserCode_CmdLoadPrioritiesLayout__Int32[]", new HashSet<CommandPermission> { CommandPermission.Manager } },
 
             // OrderPackaging
             { "OrderPackaging.InvokeUserCode_CmdActivateOrderDepartment", new HashSet<CommandPermission> { CommandPermission.General } },
@@ -166,25 +166,25 @@ public static class PermissionDatabase {
             { "PictureFrameBehaviour.InvokeUserCode_CmdChangeDefaultImage__Int32", new HashSet<CommandPermission> { CommandPermission.General } },
 
             // ProductCheckoutSpawn
-            { "ProductCheckoutSpawn.InvokeUserCode_CmdAddProductValueToCheckout", new HashSet<CommandPermission> { CommandPermission.General } },
+            { "ProductCheckoutSpawn.InvokeUserCode_CmdAddProductValueToCheckout", new HashSet<CommandPermission> { CommandPermission.Cashier } },
 
             // ProductListing
             { "ProductListing.InvokeUserCode_CmdUpdateProductPrice__Int32__Single", new HashSet<CommandPermission> { CommandPermission.General } },
             { "ProductListing.InvokeUserCode_CmdUnlockProductTier__Int32", new HashSet<CommandPermission> { CommandPermission.General } },
-            { "ProductListing.InvokeUserCode_CmdSetProductOnSale__Int32__Int32", new HashSet<CommandPermission> { CommandPermission.General } },
+            { "ProductListing.InvokeUserCode_CmdSetProductOnSale__Int32__Int32", new HashSet<CommandPermission> { CommandPermission.Manager } },
 
             // SeasonalCageEventAnimal
             { "SeasonalCageEventAnimal.InvokeUserCode_CmdHitFromPlayer__GameObject", new HashSet<CommandPermission> { CommandPermission.General } },
 
             // StolenProductSpawn
-            { "StolenProductSpawn.InvokeUserCode_CmdRecoverStolenProduct", new HashSet<CommandPermission> { CommandPermission.General } },
+            { "StolenProductSpawn.InvokeUserCode_CmdRecoverStolenProduct", new HashSet<CommandPermission> { CommandPermission.Security } },
 
             // TrashSpawn
             { "TrashSpawn.InvokeUserCode_CmdClearTrash", new HashSet<CommandPermission> { CommandPermission.General } },
 
             // UpgradesManager
             { "UpgradesManager.InvokeUserCode_CmdAddSpace__Int32", new HashSet<CommandPermission> { CommandPermission.General } },
-            { "UpgradesManager.InvokeUserCode_CmdAddStorage__Int32", new HashSet<CommandPermission> { CommandPermission.General } },
+            { "UpgradesManager.InvokeUserCode_CmdAddStorage__Int32", new HashSet<CommandPermission> { CommandPermission.Manager } },
             { "UpgradesManager.InvokeUserCode_CmdAddAddon__Int32", new HashSet<CommandPermission> { CommandPermission.General } },
             { "UpgradesManager.InvokeUserCode_CmdAcquirePerk__Int32__Int32", new HashSet<CommandPermission> { CommandPermission.General } },
             { "UpgradesManager.InvokeUserCode_CmdTimeAcceleration", new HashSet<CommandPermission> { CommandPermission.General } },
